053c9d63aa11cf71b6fe13a30aa3e14f344f4cee
Sprlnh <tuasabping@hotmail.com>
2022-04-16 12:01:46 +0700

    Lastest

--- src/App.js
+++ src/App.js
@@ -5,18 +5,25 @@ import Sidebar from "./Sidebar";
 import Feed from "./Feed";
 
 function App() {
+  const user = "test";
   return (
     <div className="app">
+      {!user ? (
+        <h1>Login</h1>
+      ) : (
+        <>
+        <Header />
 
-      <Header />
+        <div className="app__body">
+          <Sidebar />
+          <Feed/>
+        </div>
+        </>
 
-      <div className="app__body">
-        <Sidebar />
+    )}
       
-
-
-
-      </div>
+         
+   
      
     </div>
   );
